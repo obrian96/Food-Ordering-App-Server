37cd536cc1195e44b6622e88c553333637260d76
Rellik Jaeger <rellikjaeger@gmail.com>
2021-12-04 10:14:47 +0630

    auth fix

--- routes/loginrouter.js
+++ routes/loginrouter.js
@@ -15,20 +15,24 @@ loginRouter.route('/')
     next();     //continue to look for additional specification that will match /dishes
 })
 .post( async (req,res,next) => {
-    user_id = req.body.user_id;
+    user_email = req.body.user_email;
     user_pass = req.body.user_pass;
 
-    const [uname] = await db.query(`SELECT * FROM users where user_id = "${user_id}";`);
+    const [email] = await db.query(`SELECT * FROM users_details WHERE user_email = "${user_email}";`);
+    user_id = email[0].user_id;
+    const [pass] = await db.query(`SELECT * FROM users WHERE user_id = "${user_id}";`);
 
-    if(uname.length == 0){
+    // console.log(pass[0].user_pass);
+
+    if(email.length == 0){
         res.statusCode = 403;
         res.setHeader('Content-Type', 'text/json');
         res.json({"status":"false"});
     }
-    else if (uname[0].user_pass == user_pass ){
+    else if (pass[0].user_pass == user_pass){
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/json');
-        res.json(uname[0]);
+        res.json(email[0]);
     }else{
         res.statusCode = 403;
         res.setHeader('Content-Type', 'text/json');
@@ -39,12 +43,14 @@ loginRouter.route('/')
 /////////////////////////////////////////////////////////////////////////////////
 loginRouter.route('/newuser')
 .post(async (req, res, next) => {
-    const user = {  user_name: req.body.user_name,
-                    user_pass: req.body.user_pass,
-                    user_id: req.body.user_id,
-                    isAdmin: 0 
-                }
-        
+    const user = {
+        user_id: req.body.user_id,
+        user_name: req.body.user_name,
+        user_email: req.body.user_email,
+        user_pass: req.body.user_pass,
+        isAdmin: 0 
+    }
+
     const [uid] = await db.query(`SELECT * FROM users where user_id = "${user.user_id}";`);
 
     if(uid.length != 0){
@@ -53,7 +59,24 @@ loginRouter.route('/newuser')
         res.json({"status":"false"});
     }
     else {
-        db.query(`INSERT INTO users (user_id, user_name, user_pass, isAdmin) VALUES ("${user.user_id}", "${user.user_name}", "${user.user_pass}", 0);`);
+        db.query(`
+            INSERT INTO users (
+                user_id, user_name, user_pass, isAdmin
+            ) VALUES (
+                "${user.user_id}", "${user.user_name}", "${user.user_pass}", 0
+            );
+        `);
+        moment = require('moment');
+        user_joindt = moment().format("YYYY-MM-DD");
+        db.query(`
+            INSERT INTO users_details (
+                user_id, user_email, user_phno, user_addline, user_pincode, 
+                user_joindt
+            ) VALUES (
+                "${user.user_id}", "${user.user_email}", "", "", "", 
+                "${user_joindt}"
+            );
+        `);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/json');
         res.json(user);
@@ -71,7 +94,7 @@ async function main(){
       database: config.get('db.database'),
       timezone: config.get('db.timezone'),
       charset: config.get('db.charset')
-    });  
+  });  
 }
 main();
 
