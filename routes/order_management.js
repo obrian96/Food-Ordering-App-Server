1ff36bab21da6d8fe9b7bc48b283d140b1b28eaa
Rellik Jaeger <rellikjaeger@gmail.com>
2021-12-13 03:17:28 +0630

    database and server code changes

--- routes/order_management.js
+++ routes/order_management.js
@@ -22,24 +22,78 @@ orderManagementRoute.route('/')
 	res.json();
 })
 .post(async(req, res, next) => {
+	try {
 	// Check admin id
 	admin_id = req.body.admin_id;
+	if (admin_id == "" || admin_id == '[]' || admin_id == undefined) {
+		res.statusCode = 401;
+		res.setHeader('Content-Type', 'text/plain');
+		res.json({staus: "Admin id must not empty!"});
+		return;
+	}
+	const [isAdmin] = await db.query(`SELECT isAdmin FROM users WHERE user_id = "${admin_id}";`);
+	console.log(isAdmin);
+	if (isAdmin == "" || isAdmin == '[]' || isAdmin == undefined) {
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'text/plain');
+		res.json({"statue":"Account does not exist!"});
+		return;
+	}
+	if(isAdmin[0].isAdmin == 1) {
+		const [orders] = await db.query(`
+			SELECT * FROM orders;
+			`);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		res.json(orders);
+	} else {
+		res.statusCode = 401;
+		res.setHeader('Content-Type', 'text/plain');
+		res.json({"status":"Unauthorized"});
+	}
+} catch (e) {
+	console.log(e);
+}
+});
+
+orderManagementRoute.route('/ordered_user_list')
+.post(async(req, res, next) => {
 	try {
+		admin_id = req.body.user_id;
+		if (admin_id == "" || admin_id == '[]' || admin_id == undefined) {
+			res.statusCode = 401;
+			res.setHeader('Content-Type', 'text/plain');
+			res.json({status: "Admin id must not empty!"});
+			return;
+		}
 		const [isAdmin] = await db.query(`SELECT isAdmin FROM users WHERE user_id = "${admin_id}";`);
-		console.log(isAdmin);
-		if (isAdmin == []) {
+		console.log(JSON.stringify(isAdmin, null, 4));
+		if (isAdmin == "" || isAdmin == '[]' || isAdmin == undefined) {
 			res.statusCode = 404;
 			res.setHeader('Content-Type', 'text/plain');
 			res.json({"statue":"Account does not exist!"});
 			return;
 		}
 		if(isAdmin[0].isAdmin == 1) {
-			const [orders] = await db.query(`
-				SELECT * FROM orders;
+			const [ordered_user_id_list] = await db.query(`
+				SELECT MIN(id) AS id, user_id FROM orders GROUP BY user_id;
 				`);
+			console.log(ordered_user_id_list.length);
+			let user_id_list = [], user_name_list = [], user_image_list = [];
+
+			for (let i = 0; i < ordered_user_id_list.length; i++) {
+				console.log(ordered_user_id_list[i].user_id);
+				user_id_list.push("'" + ordered_user_id_list[i].user_id + "'");
+			}
+			let query = `
+				SELECT * FROM users WHERE user_id IN (${user_id_list});
+				`;
+			console.log(query);
+			const [ordered_user_list] = await db.query(query);
+			console.log(ordered_user_list);
 			res.statusCode = 200;
 			res.setHeader('Content-Type', 'text/plain');
-			res.json(orders);
+			res.json(ordered_user_list);
 		} else {
 			res.statusCode = 401;
 			res.setHeader('Content-Type', 'text/plain');
--- routes/userdetailsrouter.js
+++ routes/userdetailsrouter.js
@@ -10,81 +10,92 @@ userdetailsRouter.use(bodyParser.json());
 /////////////////////////////////////////////////////////////////////////////////
 userdetailsRouter.route('/')
 .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
     next();     //continue to look for additional specification that will match /dishes
-})
+  })
 .post( async (req,res,next) => {
-    user_id = req.body.user_id;
+  user_id = req.body.user_id;
 
-    const [uname] = await db.query(`SELECT * FROM users_details, users where users.user_id = users_details.user_id having users.user_id = "${user_id}";`);
+  const [uname] = await db.query(`SELECT * FROM users_details, users where users.user_id = users_details.user_id having users.user_id = "${user_id}";`);
 
-    if(uname.length == 0){
-        console.log("Please login again.");
-        res.statusCode = 403;
-        res.setHeader('Content-Type', 'text/json');
-        res.json({"status": "Please login again."});
-    }
-    else {
-        console.log(JSON.stringify(uname[0], null, 4));
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/json');
-        res.json(uname[0]);
-    }
-})
-.put( async (req,res,next) => {
-    const user = {user_id: req.body.user_id,
-        user_email: req.body.user_email,
-        user_phno: req.body.user_phno,
-        user_addline: req.body.user_addline,
-        user_pincode: req.body.user_pincode,
-        user_joindt: req.body.user_joindt,
-        }
-
-    db.query(`UPDATE users_details SET user_email = "${user.user_email}", user_phno = "${user.user_phno}", user_addline= "${user.user_addline}", user_pincode= "${user.user_pincode}", user_joindt = "${user.user_joindt}" where user_id = "${user.user_id}";`);
+  if(uname.length == 0){
+    console.log("Please login again.");
+    res.statusCode = 403;
+    res.setHeader('Content-Type', 'text/json');
+    res.json({"status": "Please login again."});
+  }
+  else {
+    console.log(JSON.stringify(uname[0], null, 4));
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/json');
-    res.json(user);    
+    res.json(uname[0]);
+  }
+})
+.put(async(req,res,next) => {
+  try {
+    const user = {
+      user_id: req.body.user_id,
+      user_email: req.body.user_email,
+      user_phno: req.body.user_phno,
+      user_addline: req.body.user_addline,
+      user_pincode: req.body.user_pincode,
+    }
+    // console.log(user.user_addline, user.user_pincode);
+    let sql = `UPDATE users_details SET user_email = "${user.user_email}", 
+      user_phno = "${user.user_phno}", user_addline= "${user.user_addline}", 
+      user_pincode= "${user.user_pincode}" WHERE user_id = "${user.user_id}";`;
+    const [result] = await db.query(sql);
+    console.log(result.affectedRows);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json');
+    res.json({"message": "Profile updated!"});
+  } catch (e) {
+    console.log(e);
+    res.statusCode = 401;
+    res.setHeader('Content-Type', 'text/plain');
+    res.json({status:e});
+  }
 });
 
 /////////////////////////////////////////////////////////////////////////////////
 userdetailsRouter.route('/newuser')
 .post(async (req, res, next) => {
-    const user = {user_id: req.body.user_id,
-                user_email: req.body.user_email,
-                user_phno: req.body.user_phno,
-                user_addline: req.body.user_addline,
-                user_pincode: req.body.user_pincode,
-                user_joindt: req.body.user_joindt,
-                }
-        
-    const [uid] = await db.query(`SELECT * FROM users_details where user_id = "${user.user_id}";`);
+  const user = {user_id: req.body.user_id,
+    user_email: req.body.user_email,
+    user_phno: req.body.user_phno,
+    user_addline: req.body.user_addline,
+    user_pincode: req.body.user_pincode,
+    user_joindt: req.body.user_joindt,
+  }
 
-    if(uid.length != 0){
-        res.statusCode = 409;
-        res.setHeader('Content-Type', 'text/json');
-        res.json({status:false});
-    }
-    else {
-        db.query(`INSERT INTO users_details VALUES ("${user.user_id}", "${user.user_email}", "${user.user_phno}", "${user.user_addline}", "${user.user_pincode}", '${user.user_joindt}');`);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/json');
-        res.json(user);
-    }
+  const [uid] = await db.query(`SELECT * FROM users_details where user_id = "${user.user_id}";`);
+
+  if(uid.length != 0){
+    res.statusCode = 409;
+    res.setHeader('Content-Type', 'text/json');
+    res.json({status:false});
+  }
+  else {
+    db.query(`INSERT INTO users_details VALUES ("${user.user_id}", "${user.user_email}", "${user.user_phno}", "${user.user_addline}", "${user.user_pincode}", '${user.user_joindt}');`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json');
+    res.json(user);
+  }
 })
 .delete((req, res, next) => {
-    res.end('Will add feature to delete profile later!');
+  res.end('Will add feature to delete profile later!');
 });
 
 async function main(){
-    db = await mysql.createConnection({
-      host: config.get('db.host'),
-      user: config.get('db.user'),
-      password: config.get('db.password'),
-      database: config.get('db.database'),
-      timezone: config.get('db.timezone'),
-      charset: config.get('db.charset')
-    });  
+  db = await mysql.createConnection({
+    host: config.get('db.host'),
+    user: config.get('db.user'),
+    password: config.get('db.password'),
+    database: config.get('db.database'),
+    timezone: config.get('db.timezone'),
+    charset: config.get('db.charset')
+  });  
 }
 main();
 
